1284b7e4ed154857eb237d3c1ed967598e1f3b77
Kazuya Daido <kdaido@c1r27s8.42tokyo.jp>
2023-02-11 05:58:38 +0900

    commit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,21 @@
 /*   By: kdaido <kdaido@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 00:56:44 by marvin            #+#    #+#             */
-/*   Updated: 2023/02/11 04:39:44 by kdaido           ###   ########.fr       */
+/*   Updated: 2023/02/11 05:47:51 by kdaido           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	
+	size_t dstlen;
+    size_t srclen;
+    
+    if(!dst && src && size==0)
+    return(ft_strlen(src));
+
+    dstlen=ft_strlen(dst);
+    srclen=ft_strlen(src);
+
+    
 }
--- libft.h
+++ libft.h
@@ -20,6 +20,7 @@ char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strcpy(char *dst, char *src);
 size_t	ft_strlen(const char *str);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
 char *ft_strnstr(const char *s1, const char *s2, size_t len);
 char	*ft_strrchr(const char *s, int c);
